a152f23742484a298afc087c45c7a240266c5c0f
Nishant <nishantbhosale244@gmail.com>
2021-08-13 13:47:46 +0530

    Added newtodo component and used interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+
 function App(): React.ReactElement {
 	const todos = [{ id: "t1", text: "Finish the project" }];
+
+	const todoAddHandler = (text: string) => {
+		console.log(text);
+	};
+
 	return (
 		<div className="App">
+			<NewTodo onAddTodo={todoAddHandler} />
 			<TodoList items={todos} />
 		</div>
 	);
